d79b513f816cb1aad6c31e85d379859e4f282740
metamusk <balberanjarenkim@gmail.com>
2025-03-16 18:40:19 +0800

    Adding utils extract, transform, and extrct

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
-import requests
 import os
 from dotenv import load_dotenv
-import pandas as pd
-import datetime
+from utils import extract, transform, load
 
 load_dotenv()
 
-params_geo = {"zip": "2400,PH", "appid": os.getenv('WEATHER_API')}
+api_id = os.getenv('WEATHER_API')
 
-geo_code = requests.get(f"http://api.openweathermap.org/geo/1.0/zip", params=params_geo, headers={"accepts": "application/json"})
+params_geo = {"zip": "2400,PH", "appid": api_id}
 
-geo_data = geo_code.json()
+url_geo = "http://api.openweathermap.org/geo/1.0/zip"
 
-lat = geo_data.get("lat")
+url_weather = "https://api.openweathermap.org/data/3.0/onecall/day_summary"
 
-lon = geo_data.get("lon")
+weather_data = extract(url_geo, url_weather, params_geo, api_id)
 
-df = pd.DataFrame()
+weather_df = transform(weather_data)
 
-days = 7
-
-date_today = datetime.date.today()
-week_ago = date_today - datetime.timedelta(days=days)
-
-start = datetime.date(week_ago.year, week_ago.month, week_ago.day)
-
-res = []
-
-for day in range(days):
-    date = (start + datetime.timedelta(days=day + 1)).isoformat()
-    res.append(date)
-
-rows = []
-
-for date in res:
-    weather_code = requests.get(f"https://api.openweathermap.org/data/3.0/onecall/day_summary", params={"lat": lat, "lon": lon, "date": date, "units": "metric", "exclude": "minutely,hourly,current", "appid": os.getenv("WEATHER_API")})
-    weather_data = weather_code.json()
-
-    data = {"date": weather_data.get("date"), "cloud_cover": weather_data.get("cloud_cover").get("afternoon"), "humidity": weather_data.get("humidity").get("afternoon"), "precipitation": weather_data.get("precipitation").get("total"), "min_temp": weather_data.get("temperature").get("min"), "max_temp": weather_data.get("temperature").get("max"), "max_wind_speed": weather_data.get("wind").get("max").get("speed"), "wind_direction": weather_data.get("wind").get("max").get("direction")}
-
-    rows.append(data)
-
-for i, row in enumerate(rows):
-    new_row = pd.DataFrame(row, index=[i])
-    df = pd.concat([df, new_row], ignore_index=True)
-
-print(df)
+load(weather_df)
---
+++ utils.py
@@ -0,0 +1,59 @@
+import requests
+import pandas as pd
+import datetime
+
+def extract(url_geo, url_weather, params_geo, api_id):
+    geo_code = requests.get(url_geo, params=params_geo,
+                            headers={"accepts": "application/json"})
+
+    geo_data = geo_code.json()
+
+    lat = geo_data.get("lat")
+
+    lon = geo_data.get("lon")
+
+    date_today = datetime.date.today()
+
+    week_ago = date_today - datetime.timedelta(days=7)
+
+    start = datetime.date(week_ago.year, week_ago.month, week_ago.day)
+
+    res = []
+
+    for day in range(7):
+        date = (start + datetime.timedelta(days=day + 1)).isoformat()
+        res.append(date)
+
+    rows = []
+
+    for date in res:
+        weather_code = requests.get(url_weather,
+                                    params={"lat": lat, "lon": lon, "date": date, "units": "metric",
+                                            "exclude": "minutely,hourly,current", "appid": api_id})
+
+        weather_data = weather_code.json()
+
+        data = {"date": weather_data.get("date"), "cloud_cover": weather_data.get("cloud_cover").get("afternoon"),
+                "humidity": weather_data.get("humidity").get("afternoon"),
+                "precipitation": weather_data.get("precipitation").get("total"),
+                "min_temp": weather_data.get("temperature").get("min"),
+                "max_temp": weather_data.get("temperature").get("max"),
+                "max_wind_speed": weather_data.get("wind").get("max").get("speed"),
+                "wind_direction": weather_data.get("wind").get("max").get("direction")}
+
+        rows.append(data)
+
+    return rows
+
+def transform(weather_data):
+    df = pd.DataFrame(weather_data, index=None)
+
+    print(df.shape)
+
+    return df
+
+def load(dataframe):
+    dataframe.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
+
+if __name__ == "__main__":
+    print("Don't Run")
