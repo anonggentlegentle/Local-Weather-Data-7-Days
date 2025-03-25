9d0e82535c600e7cade7160a07fc9b56d9c71a4d
metamusk <balberanjarenkim@gmail.com>
2025-03-25 20:17:53 +0800

    Adding way to check if row already exist

--- utils.py
+++ utils.py
@@ -30,17 +30,17 @@ def extract(url_geo, url_weather, params_geo, api_id):
     for date in res:
         weather_code = requests.get(url_weather,
                                     params={"lat": lat, "lon": lon, "date": date, "units": "metric",
-                                            "exclude": "minutely,hourly,current", "appid": api_id})
+                                                "exclude": "minutely,hourly,current", "appid": api_id})
 
         weather_data = weather_code.json()
 
         data = {"date": weather_data.get("date"), "cloud_cover": weather_data.get("cloud_cover").get("afternoon"),
-                "humidity": weather_data.get("humidity").get("afternoon"),
-                "precipitation": weather_data.get("precipitation").get("total"),
-                "min_temp": weather_data.get("temperature").get("min"),
-                "max_temp": weather_data.get("temperature").get("max"),
-                "max_wind_speed": weather_data.get("wind").get("max").get("speed"),
-                "wind_direction": weather_data.get("wind").get("max").get("direction")}
+                    "humidity": weather_data.get("humidity").get("afternoon"),
+                    "precipitation": weather_data.get("precipitation").get("total"),
+                    "min_temp": weather_data.get("temperature").get("min"),
+                    "max_temp": weather_data.get("temperature").get("max"),
+                    "max_wind_speed": weather_data.get("wind").get("max").get("speed"),
+                    "wind_direction": weather_data.get("wind").get("max").get("direction")}
 
         rows.append(data)
 
@@ -49,15 +49,21 @@ def extract(url_geo, url_weather, params_geo, api_id):
 def transform(weather_data):
     df = pd.DataFrame(weather_data, index=None)
 
-    print(df.shape)
-
     return df
 
 def load(dataframe):
     if os.path.exists("seven_day_local_weather_data.csv"):
-        dataframe.to_csv("seven_day_local_weather_data.csv", index=False, mode="a", header=False)
+        df = pd.read_csv("seven_day_local_weather_data.csv")
+
+        new_df = pd.concat([df, dataframe], ignore_index=True)
+
+        new_df.drop_duplicates(subset=["date"], inplace=True, keep="first")
+
+        new_df.sort_values(by="date", ascending=True, inplace=True)
+
+        new_df.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
     else:
-        dataframe.to_csv("seven_day_local_weather_data.csv", index=False, mode="a")
+        dataframe.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
 
 if __name__ == "__main__":
     print("Don't Run")
