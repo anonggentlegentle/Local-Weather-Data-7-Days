417dd1f0a383a8aec80d4731d47a717c89e1c3ed
metamusk <balberanjarenkim@gmail.com>
2025-03-29 19:03:46 +0800

    Revert "Editing dates and moving to local postgres db from csv files"
    This reverts commit 3a94bd815b26a0a8b2e29cf4519fa46d7d7a05f2.

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 import requests
 import pandas as pd
-import datetime as dt
+import datetime
 import os.path
-from datetime import datetime
-from sqlalchemy import create_engine
-import psycopg2
 
 def extract(url_geo, url_weather, params_geo, api_id):
     geo_code = requests.get(url_geo, params=params_geo,
@@ -16,16 +13,16 @@ def extract(url_geo, url_weather, params_geo, api_id):
 
     lon = geo_data.get("lon")
 
-    date_today = dt.date.today()
+    date_today = datetime.date.today()
 
-    week_ago = date_today - dt.timedelta(days=7)
+    week_ago = date_today - datetime.timedelta(days=7)
 
-    start = dt.date(week_ago.year, week_ago.month, week_ago.day)
+    start = datetime.date(week_ago.year, week_ago.month, week_ago.day)
 
     res = []
 
     for day in range(7):
-        date = (start + dt.timedelta(days=day + 1)).isoformat()
+        date = (start + datetime.timedelta(days=day + 1)).isoformat()
         res.append(date)
 
     rows = []
@@ -37,7 +34,7 @@ def extract(url_geo, url_weather, params_geo, api_id):
 
         weather_data = weather_code.json()
 
-        data = {"date": datetime.strptime(weather_data.get("date"), "%Y-%m-%d").strftime("%m-%d-%Y"), "cloud_cover": weather_data.get("cloud_cover").get("afternoon"),
+        data = {"date": weather_data.get("date"), "cloud_cover": weather_data.get("cloud_cover").get("afternoon"),
                     "humidity": weather_data.get("humidity").get("afternoon"),
                     "precipitation": weather_data.get("precipitation").get("total"),
                     "min_temp": weather_data.get("temperature").get("min"),
@@ -54,35 +51,19 @@ def transform(weather_data):
 
     return df
 
-def load(dataframe, db_user, db_pass):
-    con = create_engine(f"postgresql+psycopg2://{db_user}:{db_pass}@localhost:5432/weather_local")
+def load(dataframe):
+    if os.path.exists("seven_day_local_weather_data.csv"):
+        df = pd.read_csv("seven_day_local_weather_data.csv")
 
-    sql_df = pd.read_sql('SELECT * FROM dagupan_daily_weather', con=con)
+        new_df = pd.concat([df, dataframe], ignore_index=True)
 
-    new_df = pd.concat([sql_df, dataframe], ignore_index=True)
+        new_df.drop_duplicates(subset=["date"], inplace=True, keep="first")
 
-    new_df['date'] = pd.to_datetime(new_df['date'], errors='coerce').dt.date
+        new_df.sort_values(by="date", ascending=True, inplace=True)
 
-    new_df.drop_duplicates(subset=["date"], inplace=True, keep="first")
-
-    new_df.sort_values(by="date", ascending=True, inplace=True)
-
-    new_df.to_sql("dagupan_daily_weather", con=con, if_exists='replace', index=False)
-
-    # Old way using CSV file
-
-    # if os.path.exists("seven_day_local_weather_data.csv"):
-    #     df = pd.read_csv("seven_day_local_weather_data.csv")
-    #
-    #     new_df = pd.concat([df, dataframe], ignore_index=True)
-    #
-    #     new_df.drop_duplicates(subset=["date"], inplace=True, keep="first")
-    #
-    #     new_df.sort_values(by="date", ascending=True, inplace=True)
-    #
-    #     new_df.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
-    # else:
-    #     dataframe.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
+        new_df.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
+    else:
+        dataframe.to_csv("seven_day_local_weather_data.csv", index=False, mode="w")
 
 if __name__ == "__main__":
     print("Don't Run")
