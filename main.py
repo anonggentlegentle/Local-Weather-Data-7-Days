3a94bd815b26a0a8b2e29cf4519fa46d7d7a05f2
metamusk <balberanjarenkim@gmail.com>
2025-03-29 19:00:51 +0800

    Editing dates and moving to local postgres db from csv files

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 api_id = os.getenv('WEATHER_API')
 
+db_user = os.getenv('DB_USER')
+
+db_pass = os.getenv('DB_PASS')
+
 params_geo = {"zip": "2400,PH", "appid": api_id}
 
 url_geo = "http://api.openweathermap.org/geo/1.0/zip"
@@ -16,4 +20,4 @@ weather_data = extract(url_geo, url_weather, params_geo, api_id)
 
 weather_df = transform(weather_data)
 
-load(weather_df)
+load(weather_df, db_user, db_pass)
